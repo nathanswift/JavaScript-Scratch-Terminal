fe66b36649002b6ee5f4f263ada611edb9897536
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-08-25 17:16:59 -0600

    object and properties notes

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-function tipCalculator(bill) {
-    var percentage;
-    if (bill < 50) {
-        percentage = .2;
-    } else if (bill >= 50 && bill < 200) {
-        percentage = .15;
-    } else {
-        percentage = .1;
+// Objects and Properties
+
+//initial Object
+var nate = {
+    firstName: 'Nate',
+    lastName: 'Swift',
+    birthYear: 1993,
+    family: ['Nicki', 'Rachel', 'Jackie', 'Ron'],
+    job: 'Web Developer',
+    isMarried: false
+};
+console.log(nate);
+var x = 'birthYear';
+console.log(nate[x]);
+// console display - 1993
+
+nate.job = 'designer';
+nate['ismarried'] = true;
+console.log(nate);
+/* {firstName: "Nate", lastName: "Swift", birthYear: 1993, family: Array(4), job: "designer", …}*/
+
+// new Object syntax
+
+var nicki = new Object();
+nicki.firstName = 'Nicki';
+nicki.birthYear = 1997;
+nicki['lastName'] = 'Lowe';
+console.log(nicki);
+/* console display -
+{firstName: "Nicki", birthYear: 1997, lastName: "Lowe"} 
+(With option to drop down object values)*/
+
+// We can attach functions to objects making them 'methods'
+
+//method basics
+var nate = {
+    firstName: 'Nate',
+    lastName: 'Swift',
+    birthYear: 1993,
+    family: ['Nicki', 'Rachel', 'Jackie', 'Ron'],
+    job: 'Web Developer',
+    isMarried: false,
+    retireYear: function () {
+        this.retirement = this.birthYear + 65;
+    },
+    calcAge: function () {
+        //this references the object you're inside
+        this.age = 2018 - this.birthYear;
+        // you can pass and make new variables in your object w this.
     }
-    return percentage * bill;
-}
+};
+// call the function before console.log
+nate.calcAge();
+nate.retireYear();
+console.log(nate);
 
-var bills = [124, 48, 268];
-var tips = [tipCalculator(bills[0]),
-    tipCalculator(bills[1]),
-    tipCalculator(bills[2]),
-];
-console.log(tips);
-
-var finalBill = [ 
-    bills[0] + tips[0],
-    bills[1] + tips[1],
-    bills[2] + tips[2]
-];
-console.log(finalBill);
+/* ***READ IN HERE IF CONFUSED!!!****
+ {firstName: "Nate", lastName: "Swift", birthYear: 1993, family: Array(4), job: "Web Developer", …}
+age: 25 *** - this isn't defined in the object but in the calcAge function
+birthYear: 1993
+calcAge: ƒ ()
+family : (4) ["Nicki", "Rachel", "Jackie", "Ron"]
+firstName: "Nate"
+isMarried: false
+job: "Web Developer"
+lastName: "Swift"
+retireYear: ƒ ()
+retirement: 2058 *** - this isn't defined in the object but in the retireYear function
+__proto__: Object
+*/
