8f4c32fc30a735afebab7dd9d9d4e0d87a076247
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-08-25 15:30:02 -0600

    ⏣ created a js tip calculator

--- script.js
+++ script.js
@@ -2,21 +2,3 @@ function calculateAge(birthYear){
 	return 2018 - birthYear;
 }
 
-var ageNate = calculateAge(1993);
-var ageNicki = calculateAge(1967);
-console.log(ageNate, ageNicki);
-
-function yearsUntilRetirement(year, firstName){
-  var age = calculateAge(year);
-  var retirement = 65 - age;
-  var retirementYear = year + retirement;
-  var retirementAge = 65
-  if(retirement > 0){
-    console.log(firstName + ' will retire in ' + retirementYear + ' at a ripe old age of ' + retirementAge + ' Wow that\'s only ' + retirement + ' years away!')
-  } else{
-    console.log(firstName + ' has already retired.')
-  }
-}
-
-yearsUntilRetirement(1993, 'Nate')
-yearsUntilRetirement(1947, 'Nicki')
