645948331af6b6d4a78df3beedecf9576b65349e
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-08-25 15:30:37 -0600

    ⏣ created js tip calculator

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function calculateAge(birthYear){
-	return 2018 - birthYear;
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
 }
 
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+    tipCalculator(bills[1]),
+    tipCalculator(bills[2]),
+];
+console.log(tips);
+
+var finalBill = [ 
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+];
+console.log(finalBill);
