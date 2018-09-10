be0e1c15e6e673b7eb9a2c6d932973c3040af0a5
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-09-09 21:33:35 -0600

    ⏣ started carCalculations function

---
+++ script.js
@@ -0,0 +1,15 @@
+var car1 = 'Prius';
+var car2 = 'Taurus';
+var gasPrice = 3.15;
+var tankSize = 11.9;
+var avgMpg = 46;
+
+/* Prius's:
+        fuel capacity = 11.9 gallons
+    Taurus:
+        fuel capacity =
+*/
+var tankMiles = function() {
+    console.log(avgMpg * tankSize);
+}
+
